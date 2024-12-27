f55673ae4650aa3683021ccb7ba4c246581bc25f
박신정 <tlswjd_9015@naver.com>
2024-12-27 10:51:19 +0900

    게시판 및 댓글 조회기능

---
+++ Constants.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Constants = void 0;
+exports.Constants = {
+    BOARD: "BOARD",
+    BOARD_PREFIX: "BOARD#",
+    COMMENT_PREFIX: "COMMENT#",
+    // DynamoDB
+    EMPTY_BOARD_ITEM: {},
+};
